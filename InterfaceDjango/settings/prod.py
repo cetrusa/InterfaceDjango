ed9fdc37888c8e400673102bef936fade00c1b2f
cetrusa <cetrusa@hotmail.com>
2023-04-21 14:53:51 -0500

    cambio tur2

--- InterfaceDjango/settings/prod.py
+++ InterfaceDjango/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import sys
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['interface.amovil.co','interface.amovil.com.co','127.0.0.1','localhost']
 CSRF_TRUSTED_ORIGINS = ['https://interface.amovil.com.co']
