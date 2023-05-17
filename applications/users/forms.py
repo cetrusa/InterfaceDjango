b703cfd11d0179f2d0cde648c92706d1421bdc50
cetrusa <cetrusa@hotmail.com>
2023-05-17 09:02:42 -0500

    esta configuración solo fue para amovildesk2

--- applications/users/forms.py
+++ applications/users/forms.py
@@ -1,119 +1,96 @@
 from django import forms
 from django.contrib.auth import authenticate
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
+
+# from captcha.fields import ReCaptchaField
+# from captcha.widgets import ReCaptchaV2Checkbox
 #
 from .models import User
 
-class UserRegisterForm(forms.ModelForm):
 
+class UserRegisterForm(forms.ModelForm):
     password1 = forms.CharField(
-        label='Contraseña',
+        label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Contraseña"}),
     )
     password2 = forms.CharField(
-        label='Contraseña',
+        label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Repetir Contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Repetir Contraseña"}),
     )
-    
+
     class Meta:
         """Meta definition for Userform."""
 
         model = User
         fields = (
-            'username',
-            'email',
-            'nombres',
-            'apellidos',
-            'genero',
+            "username",
+            "email",
+            "nombres",
+            "apellidos",
+            "genero",
         )
-    
+
     def clean_password2(self):
-        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-            self.add_error('password2', 'Las contraseñas no son iguales')
+        if self.cleaned_data["password1"] != self.cleaned_data["password2"]:
+            self.add_error("password2", "Las contraseñas no son iguales")
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        label='Usuario',
+        label="Usuario",
         required=True,
         widget=forms.TextInput(
             attrs={
-                'placeholder': 'Usuario',
+                "placeholder": "Usuario",
             }
-        )
+        ),
     )
     password = forms.CharField(
-        label='Contraseña',
+        label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Contraseña"}),
     )
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+
+    # captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
     def clean(self):
         cleaned_data = super(LoginForm, self).clean()
-        username = self.cleaned_data['username']
-        password = self.cleaned_data['password']
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
 
         if not authenticate(username=username, password=password):
-            raise forms.ValidationError('Los datos de usuario no son correctos')
-        
+            raise forms.ValidationError("Los datos de usuario no son correctos")
+
         return self.cleaned_data
 
 
 class UpdatePasswordForm(forms.Form):
-
     password1 = forms.CharField(
-        label='Contraseña',
+        label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Contraseña Actual'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Contraseña Actual"}),
     )
     password2 = forms.CharField(
-        label='Contraseña',
+        label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Contraseña Nueva'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Contraseña Nueva"}),
     )
 
 
 class VerificationForm(forms.Form):
     codregistro = forms.CharField(required=True)
 
-
     def __init__(self, pk, *args, **kwargs):
         self.id_user = pk
         super(VerificationForm, self).__init__(*args, **kwargs)
 
     def clean_codregistro(self):
-        codigo = self.cleaned_data['codregistro']
+        codigo = self.cleaned_data["codregistro"]
 
         if len(codigo) == 6:
             # verificamos si el codigo y el id de usuario son validos:
-            activo = User.objects.cod_validation(
-                self.id_user,
-                codigo
-            )
+            activo = User.objects.cod_validation(self.id_user, codigo)
             if not activo:
-                raise forms.ValidationError('el codigo es incorrecto')
+                raise forms.ValidationError("el codigo es incorrecto")
         else:
-            raise forms.ValidationError('el codigo es incorrecto')
+            raise forms.ValidationError("el codigo es incorrecto")
