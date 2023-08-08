6a738e0e5e269ff8f711f8bca4d07bb6966d6b22
cetrusa <cetrusa@hotmail.com>
2023-08-08 12:06:29 -0500

    cambiado usuario docker

--- InterfaceDjango/__init__.py
+++ InterfaceDjango/__init__.py
@@ -2,6 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 
 # Esta asegurará que el app siempre se importe cuando Django se inicie
-from .configcelery import app as celery_app
+# from .configcelery import app as celery_app
 
-__all__ = ('celery_app',)
+# __all__ = ('celery_app',)
